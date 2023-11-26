a864f4befc333dd3d8432631b27ae02d7f59e2b7
nyxb <contact@nyxb.email>
2023-11-26 19:41:22 +0100

    ✨ feat(theme-shibainu): add new Vue components for theme layouts and assets
    ✨ feat(theme-shibainu): introduce SVG background components for enhanced design
    
    🎨 style(theme-shibainu): set default text color to #EBEEF2 for better contrast
    
    📚 docs(theme-shibainu): update component structure with new layout and asset files

---
+++ packages/theme-shibainu/layouts/default.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import Background from '../assets/page-6.vue'
+</script>
+
+<template>
+  <div class="kolibri-layout default-1">
+    <div class="w-[70%]">
+      <slot />
+    </div>
+    <Background class="layout-background" />
+  </div>
+</template>
+
+<style scoped>
+.kolibri-layout.default-1 {
+  color: #EBEEF2;
+}
+</style>
---
+++ packages/theme-shibainu/layouts/section.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import Background from '../assets/page-3.vue'
+</script>
+
+<template>
+  <div class="kolibri-layout section section-1 grid">
+    <div class="my-auto w-full px-10 pt-15">
+      <slot />
+    </div>
+    <Background class="layout-background" />
+  </div>
+</template>
+
+<style scoped>
+.kolibri-layout.section-1 {
+  color: #EBEEF2;
+}
+</style>
